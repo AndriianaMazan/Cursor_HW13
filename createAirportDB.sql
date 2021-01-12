1bbf0cfd9e80eb9dc1534ea142f0f6abbabb5aee
AndriianaMazan <64812682+AndriianaMazan@users.noreply.github.com>
2021-01-12 13:43:36 +0200

    modified SQL script

--- createAirportDB.sql
+++ createAirportDB.sql
@@ -24,8 +24,18 @@ CREATE TABLE IF NOT EXISTS pilots
     pilotSurname VARCHAR(15) NOT NULL,
     pilotName VARCHAR(15) NOT NULL,
     age TINYINT NOT NULL,
+    CONSTRAINT pilots_pk PRIMARY KEY (pilotID)
+);
+
+CREATE TABLE IF NOT EXISTS profReadiness
+(
+	profReadinessID serial,
+    pilotID INT NOT NULL,
     planeModel VARCHAR(20) NOT NULL,
-    CONSTRAINT pilots_pk PRIMARY KEY (pilotID),
-    CONSTRAINT pilots_models_planeModel_fk FOREIGN KEY (planeModel)
-    REFERENCES planeModels(model)
+    CONSTRAINT profReadiness_pk PRIMARY KEY (profReadinessID),
+    CONSTRAINT profReadiness_uk UNIQUE KEY (pilotID, planeModel),
+    CONSTRAINT profReadiness_models_planeModel_fk FOREIGN KEY (planeModel)
+    REFERENCES planeModels(model),
+    CONSTRAINT profReadiness_pilots_pilotID_fk FOREIGN KEY (pilotID)
+    REFERENCES pilots(pilotID)
 );
